f78243ba99a503c94bd52378cdf3d388ffc9371d
Saurav Sunil Mahajan <110589554+mahajansaurav02@users.noreply.github.com>
2023-07-24 14:56:48 +0530

    Update userRouter.js

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -6,7 +6,11 @@ const {auth}=require("../Middleware/auth")
 router.post('/auth/register',userController.registerUser)
 router.post('/auth/login',userController.loginUser)
 router.get('/users',userController.getUsers)
-router.get('/users',userController.getUsers)
+
+router.get('/users/:id',userController.getUserById)
+router.put('/users/:id',userController.updateUserById)
+router.delete('/users/:id',userController.deleteUser)
+
 
 
 module.exports=router
